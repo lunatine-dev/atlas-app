b04a76f46f2cce71117ac8f5d3dcf881e8000f26
Lunatine <github@lunatine.dev>
2026-04-01 16:54:04 +0100

    style: format pregenerated files with prettier

--- src/renderer/src/components/Versions.svelte
+++ src/renderer/src/components/Versions.svelte
@@ -1,9 +1,9 @@
 <script>
-  const versions = window.electron.process.versions
+    const versions = window.electron.process.versions;
 </script>
 
 <ul class="versions">
-  <li class="electron-version">Electron v{versions.electron}</li>
-  <li class="chrome-version">Chromium v{versions.chrome}</li>
-  <li class="node-version">Node v{versions.node}</li>
+    <li class="electron-version">Electron v{versions.electron}</li>
+    <li class="chrome-version">Chromium v{versions.chrome}</li>
+    <li class="node-version">Node v{versions.node}</li>
 </ul>
